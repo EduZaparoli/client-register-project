7f2b7da426b01b6bfae3e8be63acf52b3bf167e1
Eduardo Zaparoli <eduzzx1204@gmail.com>
2022-05-24 15:45:26 -0300

    Creating function to generate hash password

--- src/models/Cliente.js
+++ src/models/Cliente.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 //schema do db
 const clienteSchema = new mongoose.Schema(
@@ -6,12 +7,19 @@ const clienteSchema = new mongoose.Schema(
         id: {type: String},
         nome: {type: String, required: true},
         email: {type: String, required: true},
-        senha: {type: String, required: true}
+        senha: {type: String, required: true, select: false}
     },
     {
         versionKey: false
     }
 )
+
+clienteSchema.pre('save', async function(next) {
+    const hash = await bcrypt.hash(this.senha, 10);
+    this.senha = hash;
+
+    next();
+});
 
 const clientes = mongoose.model("clientes", clienteSchema);
 
