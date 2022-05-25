6211d36a3df055a72790c5e2328ebc6ce3977c31
Eduardo Zaparoli <eduzzx1204@gmail.com>
2022-05-25 15:02:17 -0300

    Update on get routes

--- src/routes/clientesRoutes.js
+++ src/routes/clientesRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import ClienteController from "../controllers/clientesController.js";
+import autenticaToken from "../midlewares/authMidleware.js";
 
 //constante para definir a rota do express
 const router = express.Router();
 
 //oque vai acontecer na rota
 router
-    .get("/clientes", ClienteController.listarClientes)
+    .get("/clientes", autenticaToken, ClienteController.listarClientes)
     .post("/clientes", ClienteController.cadastrarCliente)
     .post("/clientes/autenticar", ClienteController.autenticarCliente)
     .put("/clientes/id:", ClienteController.atualizarCliente)
